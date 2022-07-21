f56d7b4a2b6950e142869876b82ef4112814ca69
Martina <84615761+martspyg@users.noreply.github.com>
2022-07-21 16:42:40 +0700

    remote rest template configuration

--- src/main/java/com/project/books/configuration/ApplicationConfiguration.java
+++ src/main/java/com/project/books/configuration/ApplicationConfiguration.java
@@ -3,15 +3,9 @@ package com.project.books.configuration;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class ApplicationConfiguration {
-
-	@Bean
-	RestTemplate restTemplate() {
-		return new RestTemplate();
-	}
 	
 	@Bean
 	public ModelMapper modelMapper() {
